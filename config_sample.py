a007a65835c30271a516ca6f31217509ade91a91
Sergio Matos <114961377+sergio-matos-eng@users.noreply.github.com>
2026-02-05 17:32:31 -0300

    Update sample config values for router settings

--- config_sample.py
+++ config_sample.py
@@ -1,5 +1,5 @@
 # config_sample.py
 # Rename this file to config.py and update the values below
-ROUTER_IP = "ROUTER IP HERE"
-USERNAME = "USERNAME HERE"
-PASSWORD = "PASSWORD HERE" 
+ROUTER_IP = "YOUR_ROUTER_IP_HERE"
+USERNAME = "YOUR_SECURE_USERNAME_HERE"
+PASSWORD = "YOUR_SECURE_PASSWORD_HERE"
